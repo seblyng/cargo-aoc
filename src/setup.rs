13bedca5366b6d4f055ed79aa5859deae7c62262
Sivert Johansen <sivert-joh@hotmail.com>
2025-12-21 21:13:51 +0100

    fix: .env file should be in the year folder

--- src/setup.rs
+++ src/setup.rs
@@ -1,16 +1,17 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::ArgMatches;
 
 use crate::error::AocError;
 
-async fn setup_template_project(year: i32) -> Result<(), AocError> {
+async fn setup_template_project(year: i32) -> Result<PathBuf, AocError> {
     if Path::new(&format!("{year}")).exists() {
         return Err(AocError::SetupExists);
     }
 
     let year = format!("{}", year);
-    tokio::fs::create_dir(&year).await?;
+    let path = Path::new(&year);
+    tokio::fs::create_dir(&path).await?;
 
     let template_dir = format!("{}/template", env!("CARGO_MANIFEST_DIR"));
 
@@ -28,10 +29,10 @@ async fn setup_template_project(year: i32) -> Result<(), AocError> {
         )
         .await?;
     }
-    Ok(())
+    Ok(path.to_path_buf())
 }
 
-async fn get_session_token() -> Result<(), AocError> {
+async fn get_session_token(year_path: &Path) -> Result<(), AocError> {
     if dotenv::var("AOC_TOKEN").is_err() {
         println!("Paste session token here for automatic download of input files");
         let mut input = String::new();
@@ -39,7 +40,7 @@ async fn get_session_token() -> Result<(), AocError> {
         let input = input.trim();
 
         if !input.is_empty() {
-            let env_file = std::env::current_dir()?.join(".env");
+            let env_file = year_path.join(".env");
             tokio::fs::write(env_file, format!("AOC_TOKEN={input}"))
                 .await
                 .expect("Couldn't write to file");
@@ -62,7 +63,7 @@ fn get_year(matches: &ArgMatches) -> Result<i32, AocError> {
 pub async fn setup(args: &ArgMatches) -> Result<(), AocError> {
     let year = get_year(args)?;
 
-    setup_template_project(year).await?;
-    get_session_token().await?;
+    let path = setup_template_project(year).await?;
+    get_session_token(&path).await?;
     Ok(())
 }
